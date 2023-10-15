c9b3b162b07a54ce67b0d037e1528c20cf8c120a
박건우 <136959177+chaos1231107@users.noreply.github.com>
2023-10-15 10:51:58 +0900

    Add files via upload

---
+++ bmp_servo_file.py
@@ -0,0 +1,39 @@
+import time
+import board
+import busio
+import digitalio
+import adafruit_bmp280
+import RPi.GPIO as GPIO
+
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.D5)
+bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, cs)
+bmp280.sea_level_pressure = 1011.
+GPIO.setmode(GPIO.BCM)
+servo_pin = 18
+GPIO.setup(servo_pin, GPIO.OUT)
+GPIO.setwarnings(False)
+pwm = GPIO.PWM(servo_pin, 50)
+pwm.start(0)
+
+while True:
+    altitude = bmp280.altitude
+    temperature = bmp280.temperature
+    print("Altitude: ", altitude)
+    # file append mode 
+    with open('bmp_data.txt', 'a') as file:
+        file.write(f"Altitude: {altitude} m\n")
+        file.flush()
+        time.sleep(0.5)
+    if altitude >= 65.0:
+        try:
+            pwm.ChangeDutyCycle(12.5)  # 12.5 means 180 degrees
+            time.sleep(1)  # program waits for 1 second
+            pwm.ChangeDutyCycle(2.5)  # Change to 0 degrees (0.5ms pulse width)
+            time.sleep(0.5)  # program waits for 0.7 second
+
+        except KeyboardInterrupt:
+            pass
+        finally:
+            pwm.stop()
+            GPIO.cleanup()
